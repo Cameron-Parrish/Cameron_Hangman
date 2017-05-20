648a19ce4c6775af569655e0c2c9597ad59d5a6f
Cameron-Parrish <17parcam@ths.school.nz>
2017-05-20 14:17:03 +1200

    Cameron Program v2
    Various tweaks made. Can now insert a players guess into the blanks list
    which is compared with the secrete word to determain if the player has
    guessed the word. the guess_add function cannot however deal with words
    containing duplicate letters.

---
+++ Cameron Program v2.py
@@ -0,0 +1,99 @@
+#Super early version 2
+#Has deviated far from the plan, only now relaising how terrible it is.
+
+import random
+import string
+
+def force_number():#This function uses try/except block to only allow the use to enter a number < 0 or > 3
+    amount_of_games = 0
+    while amount_of_games > 3 or amount_of_games <  1:
+        try:
+            amount_of_games = int(input("Enter the amount of games to play (max 3)"))#This
+        except:
+            print("Please enter a valid integer")
+        else:
+            print()
+    print("You have chosen {} amount of games".format(amount_of_games))
+    return amount_of_games
+
+def force_letter():#This function checks to make sure that only a single letter guess is entered, and that it is a lower case letter
+    guess = ("")
+    while  str.isalpha(guess) == False or len(guess) > 1:#If the imported guess variable contains no ap=lpha characters or the user input is > 1, keep asking the user for a single letter guess
+            guess = input("Please enter a single letter guess")
+            print(guess)
+    return guess
+
+
+def guess_add(guess,blanks):#Determains index position of guess in secret word and inserts it into blanks. this however creates an extra element which needs to be deleted.
+    index = secret_word.index(guess)
+    blanks.insert(index,guess)
+    del blanks[-1]
+    return blanks
+
+
+for i in range(force_number()):#Call function force_number to ask user how many games they want, use a for in range loop to run the game that many times
+    guessed_list = []
+    word_list = ["campbell", "pie","doom"]
+    random.shuffle(word_list)
+    secret_word = word_list[0]
+    secret_word = list(secret_word)#Converts the secret word to a list as strings are immutable
+    blanks = len(secret_word) * ("_")
+    blanks = list(blanks)
+    lives = 10
+    repeat_guess = True
+    guess = ("")#This triggers the force_letter function to ask the user to enter a letter, this is because an empty string variable contains no alpha characters
+    print(secret_word)
+
+    while repeat_guess == True:#This loop checks the user input to make sure that it is a letter present in the word. This means that the guess_check function does'nt need to check if the letter is in the word and instead add the letter to blanks
+        guess = force_letter()
+        if guess in guessed_list:
+            print("You have already guessed that")
+        elif guess not in secret_word:
+            guessed_list.append(guess)#append the guessed letter to the guessed_list to ensure that it isn't guessed again by the user
+            lives -= 1
+            print("You have {} lives less child".format(lives))
+            print("")
+            print(guessed_list)
+        elif guess in secret_word:
+            blanks = guess_add(guess, blanks)#Imports the guess and blanks variables(both global) into the guess_add function so that the guess variable can be inserted into blanks.
+            print("You have chosen wisely")
+            print(blanks)
+        if blanks == secret_word:
+            repeat_guess = False
+        if lives < 0:
+            print("You lose :(")
+            repeat_guess = False
+    
+
+    
+    
+
+
+
+
+    
+
+
+
+    
+
+
+    
+
+    
+
+
+
+    
+
+
+
+        
+        
+        
+
+    
+        
+
+                                  
+    
